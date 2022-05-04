71f57b271c68234862f16ec815386aab06583e02
revers3ntropy <josephcoppin@gmail.com>
2022-05-04 14:39:22 +0100

    tensorflow in python

--- tensorflow.js/train.js
+++ tensorflow.js/train.js
@@ -2,32 +2,34 @@ import * as tf from '@tensorflow/tfjs-node';
 import * as fs from 'fs';
 import now from 'performance-now';
 
-const ALPHA = 0.001,
+const ALPHA = 0.02,
 	EPOCHS = 100,
 	BATCH_SIZE = 350,
 	IMG_X = 28,
-	IMG_Y = 28;
+	IMG_Y = 28,
+	HIDDEN_SIZE = 4,
+	NUM_DIGITS = 10
 
 function getData () {
 	let [X, y] = JSON.parse(String(fs.readFileSync('../train.json')));
 	let [X_test, y_test] = JSON.parse(String(fs.readFileSync('../test.json')));
 
 	y = y.map(v => {
-		let arr = Array(10).fill(0);
+		let arr = Array(NUM_DIGITS).fill(0);
 		arr[v] = 1;
 		return arr;
 	});
 
 	y_test = y_test.map(v => {
-		let arr = Array(10).fill(0);
+		let arr = Array(NUM_DIGITS).fill(0);
 		arr[v] = 1;
 		return arr;
 	});
 
-	y = tf.tensor(y).reshape([X.length, 10]);
+	y = tf.tensor(y).reshape([X.length, NUM_DIGITS]);
 	X = tf.tensor(X, [X.length, IMG_X, IMG_Y]);
 
-	y_test = tf.tensor(y_test).reshape([X_test.length, 10]);
+	y_test = tf.tensor(y_test).reshape([X_test.length, NUM_DIGITS]);
 	X_test = tf.tensor(X_test, [X_test.length, IMG_X, IMG_Y]);
 
 	return [X, y, X_test, y_test];
@@ -35,8 +37,6 @@ function getData () {
 
 async function main () {
 	const [X, y, X_test, y_test] = getData();
-
-	const HIDDEN_SIZE = 4
 	const model = tf.sequential();
 
 	model.add(
@@ -55,7 +55,7 @@ async function main () {
 	model.add(tf.layers.flatten());
 	model.add(
 		tf.layers.dense({
-			units: 10,
+			units: NUM_DIGITS,
 		})
 	);
 
@@ -86,6 +86,10 @@ async function main () {
 	});
 
 	console.log(times.reduce((a, b) => a + b, 0) / times.length, 'ms on average');
+
+	await model.save('file:///home/joseph/dev/mnist/tensorflow.js/model');
+
+	console.log('Saved model');
 }
 
 main();

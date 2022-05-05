02ea496b76bf73d053736e5e51de610829abf4b6
revers3ntropy <josephcoppin@gmail.com>
2022-05-05 08:01:22 +0100

    trying to improve accuracy

--- frontend/index.js
+++ frontend/index.js
@@ -1,51 +1,25 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
 const result = document.getElementById('result');
+const prediction = document.getElementById('prediction');
+const imgDiv = document.getElementById('image');
 
 let dragging = false;
 const pos = [0, 0];
 
+tf ??= tensorflow;
+
 let model;
 
-canvas.addEventListener('mousedown',  engage);
-canvas.addEventListener('mousedown',  setPosition);
-canvas.addEventListener('mousemove',  draw);
-canvas.addEventListener('mouseup', disengage);
-
-// touch
-canvas.addEventListener('touchstart', engage);
-canvas.addEventListener('touchmove', setPosition);
-canvas.addEventListener('touchmove', draw);
-canvas.addEventListener('touchend', disengage);
-
-function isTouchDevice() {
-  return (
-        ('ontouchstart' in window) ||
-        (navigator.maxTouchPoints > 0) ||
-        (navigator['msMaxTouchPoints'] ?? 0 > 0)
-    );
-}
-
-function engage () {
+canvas.addEventListener('mousedown', () => {
     dragging = true;
-}
-
-function disengage () {
+});
+canvas.addEventListener('mouseup', () => {
     dragging = false;
-}
+});
 
-function setPosition (e) {
-    if (isTouchDevice()) {
-        const touch = e.touches[0];
-        pos.x = touch.clientX - ctx.canvas.offsetLeft;
-        pos.y = touch.clientY - ctx.canvas.offsetTop;
-    } else {
-        pos.x = e.clientX - ctx.canvas.offsetLeft;
-        pos.y = e.clientY - ctx.canvas.offsetTop;
-    }
-}
-
-function draw (e) {
+canvas.addEventListener('mousedown', setPosition);
+canvas.addEventListener('mousemove', async (e) => {
     e.preventDefault();
     e.stopPropagation();
 
@@ -53,46 +27,85 @@ function draw (e) {
 
     ctx.beginPath();
 
-    ctx.lineWidth = 40;
+    ctx.lineWidth = 20;
     ctx.lineCap = 'round';
-    ctx.strokeStyle = 'red';
+    ctx.strokeStyle = 'rgb(16,190,190)';
 
     ctx.moveTo(pos.x, pos.y);
     setPosition(e);
     ctx.lineTo(pos.x, pos.y);
 
     ctx.stroke();
+    predictModel();
+});
+
+function setPosition (e) {
+    pos.x = e.clientX - ctx.canvas.offsetLeft;
+    pos.y = e.clientY - ctx.canvas.offsetTop;
 }
 
 // clear canvas
 function erase () {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    predictModel();
 }
 
 async function loadModel () {
-    model = await tf.loadLayersModel('../tensorflow.js/model');
+    model = await tf.loadLayersModel('../tensorflow.js/model/model.json');
     // warm up
-    model.predict(tf.zeros([1, 28, 28, 1]))
+    model.predict(tf.zeros([1, 28, 28]))
 }
 
-function getData(){
+function getData () {
     return ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-async function predictModel() {
+async function predictModel () {
     const imageData = getData();
 
     // converts from a canvas data object to a tensor
     let image = tf.browser.fromPixels(imageData)
 
     // pre-process image
-    image = tf.image.resizeBilinear(image, [28,28]).sum(2).expandDims(0).expandDims(-1)
+    image = tf.image.resizeBilinear(image, [28,28]).sum(2).expandDims(0);
+
+    let imgDivHTML = '';
+    for (let row of (await image.array())[0]) {
+        for (let n of row) {
+            imgDivHTML += `
+                <div style="background: rgb(${n}, ${n}, ${n})"></div>
+            `;
+        }
+    }
+    imgDiv.innerHTML = imgDivHTML;
 
     // gets model prediction
-    const y = model.predict(image);
+    const y = (await model.predict(image).array())[0];
 
-    // replaces the text in the result tag by the model prediction
-    result.innerHTML = y.argMax(1).dataSync();
+    prediction.innerHTML = '';
+
+    let max = 0;
+    let maxV = -Infinity;
+
+    for (let i = 0; i < 10; i++) {
+        if (y[i] > maxV) {
+            max = i;
+            maxV = y[i];
+        }
+
+        if (y[i] < 0) y[i] = 0;
+
+        prediction.innerHTML += `
+            <div style="width: 100%; margin: 10px;">
+                <div class=bar style="width: ${y[i] * 1000}px"> </div>
+                <span style="float: left; transform: translate(20px, -20px)">
+                    ${i}: ${(y[i] * 100).toFixed(2)}%
+                </span>
+            </div>
+        `;
+    }
+
+    result.innerText = max.toString();
 }
 
 loadModel();
--- tensorflow.js/train.js
+++ tensorflow.js/train.js
@@ -2,26 +2,41 @@ import * as tf from '@tensorflow/tfjs-node';
 import * as fs from 'fs';
 import now from 'performance-now';
 
-const ALPHA = 0.02,
+const ALPHA = 0.005,
 	EPOCHS = 100,
-	BATCH_SIZE = 350,
+	BATCH_SIZE = 300,
 	IMG_X = 28,
 	IMG_Y = 28,
-	HIDDEN_SIZE = 4,
-	NUM_DIGITS = 10
+	HIDDEN_SIZE = 16,
+	NUM_DIGITS = 10;
+
+function max (arr) {
+	let maxIdx = -1;
+	let maxV = -Infinity;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > maxV) {
+			maxIdx = i;
+			maxV = arr[i];
+		}
+	}
+
+	return [ maxIdx, maxV ];
+}
+
 
 function getData () {
 	let [ X, y ] = JSON.parse(String(fs.readFileSync('../train.json')));
 	let [ X_test, y_test ] = JSON.parse(String(fs.readFileSync('../test.json')));
 
 	y = y.map(v => {
-		let arr = Array(NUM_DIGITS).fill(0);
+		let arr = Array(NUM_DIGITS).fill(-.1);
 		arr[v] = 1;
 		return arr;
 	});
 
 	y_test = y_test.map(v => {
-		let arr = Array(NUM_DIGITS).fill(0);
+		let arr = Array(NUM_DIGITS).fill(-.1);
 		arr[v] = 1;
 		return arr;
 	});
@@ -32,7 +47,32 @@ function getData () {
 	y_test = tf.tensor(y_test).reshape([X_test.length, NUM_DIGITS]);
 	X_test = tf.tensor(X_test, [X_test.length, IMG_X, IMG_Y]);
 
-	return [X, y, X_test, y_test];
+	return [ X, y, X_test, y_test ];
+}
+
+async function test (model, X_test, y_test) {
+	const n = Math.min(1000, X_test.shape[0]);
+
+	y_test = await y_test.array();
+	X_test = await X_test.array();
+
+	let totalError = 0;
+	let incorrects = 0;
+
+	for (let i = 0; i < n; i++) {
+		const X = tf.tensor(X_test[i]).reshape([1, 28, 28]);
+		let y = (await model.predict(X).array())[0];
+		for (let j = 0; j < NUM_DIGITS; j++) {
+			totalError += (y[j] - y_test[i][j])**2;
+		}
+
+		if (max(y)[0] !== max(y_test[i])[0]) {
+			incorrects++;
+		}
+	}
+
+	console.log(`${incorrects}/${n} incorrect`);
+	console.log(`${totalError/n} avg error`);
 }
 
 async function main () {
@@ -76,7 +116,9 @@ async function main () {
 				losses.push(logs.loss);
 				times.push(now() - start);
 				start = now();
-				console.log(`Finished epoch ${epoch+1} / ${EPOCHS}: ${logs.loss}`);
+				if (epoch % 100 === 0 || epoch === EPOCHS) {
+					console.log(`Finished epoch ${epoch+1} / ${EPOCHS}: ${logs.loss}`);
+				}
 			},
 		},
 		batchSize: BATCH_SIZE,
@@ -85,7 +127,9 @@ async function main () {
 		validationData: [X_test, y_test]
 	});
 
-	console.log(times.reduce((a, b) => a + b, 0) / times.length, 'ms on average');
+	console.log((times.reduce((a, b) => a + b, 0) / times.length).toFixed(5), 'ms on average');
+
+	await test(model, X_test, y_test);
 
 	await model.save('file:///home/joseph/dev/mnist/tensorflow.js/model');
 
